7d7410edc3e5025a8e018f0a09b2fb07674730aa
Ilona Stutz <ilona.stutz@gmail.com>
2013-10-02 15:11:56 +0200

    done! done with everything excl. stylo.

--- c5c.js
+++ c5c.js
@@ -1,135 +1,173 @@
 ;(function ( document, window ) {
 
-    // arrays for people and chores. there have to be as many people as chores.
-    var people = [ "Joel", "Carola", "Sebu", "Michèle", "Paolo", "Lisa", "Timo", "Ilona" ],
-        chores = [ "Zimmer", "Joker*", "Küche", "1. Stock", "Kühlschrank", "Diverses", "Entsorgung", "Boden" ],
-        schedule = [],
-        startDate = new Date( '2013-07-25' ),
+    var generator = document.getElementById( 'generator' ),
         container = document.getElementById( 'container' ),
-        // last chores: 0: "Zimmer" "Diverses" "1. Stock" "Küche" "Kühlschrank" "Joker*" "Boden" "Entsorgung"
+        archive = document.getElementById( 'archive' ),
+        URLPATH = 'http://127.0.0.1:8080/',
 
         /**
-         * Create the array with all four dates based on the startdate.
-         * @param {Date} startDate - the next date the chores should be done.
-         * @return {Array} - contains the next four dates at weekly intervals.
-         */
-        generateSchedule = function ( startDate ) {
-            var newDate,
-                next,
-                schedule = [],
-                i;
-
-            for ( i = 0; i < 4; i++ ) {
-                // TODO:
-                // making a copy would be nicer. is Object.create(starDate) an option?
-                // so said it's not a 'deep' copy. do i care? try it. deep copy object.
-                newDate = new Date( startDate.toString() );
-                next = startDate.getDate() + ( i * 7 );
-                newDate.setDate( next );
-                schedule.push( newDate.toDateString() );
-            }
-
-            return schedule;
-        },
-
-        /**
-         * Randomize chores and push them to an array iff they are different
-         * in every index relative to the old chores distribution. Do this until
-         * the array contains four entries (= weekly chores for one month).
-         * Note: The condition only applies to consecutive chores distributions -
-         * it is fine if two nonconsecutive chores distributions have the
-         * same value for the same index.
-         * @param  {Array} chores - chores in the order of their last distribution.
-         * @return {Array} - a 2D array containing four randomized chore distributions.
-         */
-        randomizeChores = function ( chores ) {
-            var randomizedChores = [],
-                choresNew = chores.slice(),
-                tmpChores = chores.slice(),
-                randomize = function () {
-                    return 0.5 - Math.random();
-                };
-
-            while ( randomizedChores.length < 4 ) {
-                choresNew.sort( randomize );
-
-                if ( !isRepetitive( tmpChores, choresNew ) ) {
-                    tmpChores = choresNew.slice();
-                    randomizedChores.push( tmpChores );
-                }
-            }
-
-            return randomizedChores;
-        },
-
-        /**
-         * Helper to determine if there are any repetitive elements in two
-         * chores arrays, i.e. if they have the same value for the same
-         * index at least once.
-         * @param {Array} choresOld - old chore distribution.
-         * @param {Array} choresNew - randomized chore distribution.
-         * @return {Boolean} - true if there are repetitive elements, else undefined.
-         */
-        isRepetitive = function ( choresOld, choresNew ) {
-            for ( var i = 0; i < choresOld.length; i++ ) {
-                if ( choresOld[ i ] === choresNew[ i ] ) {
-                    return true;
-                }
-            }
-        },
-
-        /**
-         * Create HTML table from data - first insert dates horizontally,
-         * then people and randomizedChores vertically.
-         * Use innerHTML as it is faster than pure DOM.
-         * @param {Array} schedule - chore deadline dates for the table header.
-         * @param {Array} people - names of chore-doers for the first column.
-         * @param {Array} randomizedChores - contains the chores arrays.
+         * Creates HTML table from row arrays.
+         * First inserts dates as a header, then people and randomized chores.
+         * @param {Array} data - array of arrays where each corresponds to a row.
          * @return {String} - table as a string to be inserted using innerHTML.
          */
-        createTable = function ( schedule, people, radomizedChores ) {
-            var table = "<table>",
-                column,
-                i;
+        createTable = function ( data ) {
+            var table = '<table>',
+                i,
+                j;
 
-            // add the schedule as a header
-            table += "<tr>";
-            table += "<th></th>"; // first cell of the table is empty
-            for ( i = 0; i < schedule.length; i++ ) {
-                table += "<th>" + schedule[ i ] + "</th>";
+            // add the first row as a header
+            table += '<tr>';
+            table += '<th></th>'; // first cell of the table is empty
+            for ( i = 0; i < data[ 0 ].length; i++ ) {
+                table += '<th>' + data[ 0 ][ i ] + '</th>';
             }
-            table += "</tr>";
+            table += '</tr>';
 
             // add the people and chores
-            randomizedChores.unshift( people ); // prepend chores array by people
-            for ( i = 0; i < randomizedChores[0].length; i++ ) {
-                table += "<tr>";
-                for ( j = 0; j < randomizedChores.length; j++ ) {
-                    column = randomizedChores [ j ];
-                    table += "<td>" + column[ i ] + "</td>";
+            for ( i = 1; i < data.length; i++ ) {
+                table += '<tr>';
+                for ( j = 0; j < data[ i ].length; j++ ) {
+                    table += '<td>' + data[ i ][ j ] + '</td>';
                 }
-                table += "</tr>";
+                table += '</tr>';
             }
-            table += "</table>";
+            table += '</table>';
 
             return table;
+        },
+
+        /**
+         * Handler for click on "Generate table" button.
+         * POSTs to 'schedules' and either gets a new table or a "too early"-msg
+         * in return. Either inserts the new table as inner HTML or alerts the
+         * user using the msg.
+         * @param  {Event} e - submit "Generate table" form.
+         * @return false to prevent propagation.
+         */
+        processForm = function ( e ) {
+            if (e.preventDefault) e.preventDefault();
+            httpPost( URLPATH + 'schedules');
+            return false;
+        },
+
+        /**
+         * Processes a GET request to the given destination url.
+         */
+        httpGet = function ( url ) {
+            var xhr = new XMLHttpRequest();
+            xhr.open( 'GET', url, false );
+            xhr.send();
+            return xhr.responseText;
+        },
+
+        /**
+         * Reloads the archive and inserts the new table / alert msg into the HTML file.
+         */
+        processResponse = function ( event )  {
+            var xhrResponse = event.target,
+                resp,
+                data;
+
+            if ( xhrResponse.readyState === 4 ) {
+                if ( xhrResponse.status === 200 ) {
+                    generateArchive( URLPATH + 'schedules' );
+                    clickifyArchive();
+                }
+
+                resp = JSON.parse( xhrResponse.response );
+
+                if ( resp.data ) {
+                    data = createTable( resp.data );
+                } else {
+                    data = resp.message;
+                }
+                container.innerHTML = data;
+                printify();
+            }
+        },
+
+        /**
+         * Processes a POST request asynchronously to the given destination url.
+         * Uses processResponse to display the new data.
+         * @param  {String} url - destination url for POST request.
+         */
+        httpPost = function ( url ) {
+            var xhr = new XMLHttpRequest();
+
+            xhr.onreadystatechange = processResponse;
+            xhr.open( 'POST', url, true );
+            xhr.setRequestHeader( 'Content-type', 'application/x-www-form-urlencoded' );
+            xhr.send();
+        },
+
+        /**
+         * Fetches all dates of past chores plans and inserts them into the HTML
+         * file as a list.
+         * @param  {String} url - destination url for GET request.
+         */
+        generateArchive = function ( url ) {
+            var responseArray = JSON.parse( httpGet( url )),
+                list = '<h3>Check out the old plans</h3>';
+
+            list += '<ul>';
+            for ( i = 0; i < responseArray.length; i++ ) {
+                list += '<li><a href="#">' + responseArray[ i ].date + '</a></li>';
+            }
+            list += '</li>';
+
+            archive.innerHTML = list;
+        },
+
+        /**
+         * Fetches the table corresponding to the date which was clicked on in
+         * the archive and inserts it into the HTML file.
+         * @param  {Event} e - click on a list item from the archive.
+         */
+        fetchTable = function ( e ) {
+            if (e.preventDefault) e.preventDefault();
+            var date = e.target.innerHTML,
+                url = URLPATH + 'schedules/' + date;
+            container.innerHTML = createTable( JSON.parse( httpGet( url )).data );
+            printify();
+            return false;
+        },
+
+        /**
+         * Binds the archive items to a click event which will trigger the
+         * fetchTable function.
+         */
+        clickifyArchive = function () {
+            var listItems = document.getElementsByTagName( 'a' ),
+                i;
+            for ( i = 0; i < listItems.length; i++ ) {
+                listItems[ i ].addEventListener( 'click', fetchTable );
+            }
+        },
+
+        /**
+         * Pops up the print window.
+         */
+        print = function ( e ) {
+            if (e.preventDefault) e.preventDefault();
+            window.print();
+            return false;
+        },
+
+        /**
+         * Adds a print button iff there is a table.
+         */
+        printify = function () {
+            var printer;
+            if ( document.getElementsByTagName( 'table' )) {
+                container.insertAdjacentHTML('beforeend', '<form id="printer"><input type="button" value="Print"></form>' );
+                printer = document.getElementById( 'printer' );
+                printer.addEventListener( 'click', print );
+            }
         };
 
-
-    schedule = generateSchedule( startDate );
-    randomizedChores = randomizeChores( chores );
-    table = createTable( schedule, people, randomizedChores );
-    container.innerHTML = table;
-
-    // debug
-    console.log( schedule );
-    console.log( randomizedChores );
-    console.log( table );
-
-    // TODO: beautify the table generator function
-    // TODO: add input fields for date, names, chores and 'generate' button.
-    // TODO : dump last chores order to file to reuse as first one next time
-    // TODO: create printing functionality
-
+    generateArchive( URLPATH + 'schedules' );
+    clickifyArchive();
+    generator.onsubmit = processForm;
 
 }( document, window ));
