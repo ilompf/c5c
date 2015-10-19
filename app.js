4219e8b1ff6ed6b82ade85bdbbc26b9b28fb565b
ilompf <ilona.stutz@gmail.com>
2015-10-19 11:19:30 +0200

    CORS quickfix & new names

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var restify = require( 'restify' ),
                 lastDate = new Date( results[ 0 ].date );
 
             if ( isGenerationAllowed( lastDate )) {
-                var people = [ 'Joel', 'Carola', 'Sebu', 'Michèle', 'Coco', 'Sophie', 'Timo', 'Simon' ],
+                var people = [ 'Joel', 'Noëmi', 'Sebu', 'Michèle', 'Janna', 'Selda', 'Timo', 'X' ],
                     schedule = generateSchedule( lastDate ),
                     randomizedChores = randomizeChores( lastChores ),
                     table = createTable( schedule, people, randomizedChores ),
@@ -238,6 +238,11 @@ var restify = require( 'restify' ),
         }
     };
 
+
+server.opts(/\.*/, function (req, res, next) {
+	res.send(200);
+	next();
+});
 
 server.use( restify.acceptParser( server.acceptable ));
 server.use( restify.bodyParser() );
